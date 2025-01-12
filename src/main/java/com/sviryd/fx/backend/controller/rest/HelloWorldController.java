8a9d533cb9886c27c06c003a7ff434f64148bbee
mgsviryd <mgsviryd@gmail.com>
2025-01-12 17:44:20 +0300

    server: set baseUrl for server

--- src/main/java/com/sviryd/fx/backend/controller/rest/HelloWorldController.java
+++ src/main/java/com/sviryd/fx/backend/controller/rest/HelloWorldController.java
@@ -1,9 +1,11 @@
 package com.sviryd.fx.backend.controller.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello-world")
 public class HelloWorldController {
     @GetMapping
     public String helloWorld() {
